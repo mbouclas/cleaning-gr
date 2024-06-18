6ce0b94d428f76280ae435e17a11b3e53e64fec3
Michael Bouclas <mbouclas@gmail.com>
2024-06-18 16:23:46 +0300

    Testimonials

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -2,9 +2,6 @@
     import SideBar from '@components/sidebar.svelte';
 </script>
 <header>
-    <div class="md:hidden">
-
-    </div>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="/" title="Cleaning.gr Αρχική">
